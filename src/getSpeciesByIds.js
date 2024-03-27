b91fd3ef053f7f096fc451fb3780e6a03b5ed136
Jonatan Crespo <jonatan.domingues.crespo@gmail.com>
2024-03-26 23:32:35 -0300

    Começando a trabalhar na primeira função

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  ids.find((specie) => {
+    if(ids.includes(specie.id))
+    return specie;
+    // console.log(specie);
+  });
+  console.log(ids);
 };
+
+getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae');
+
 
 module.exports = getSpeciesByIds;

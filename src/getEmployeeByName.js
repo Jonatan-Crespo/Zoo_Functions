5cf2a19fed9911e0048318b96db24777907829e8
Jonatan Crespo <jonatan.domingues.crespo@gmail.com>
2024-04-16 11:37:26 -0300

    Finalizando a função e o teste do arquivo getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => {
-  // seu código aqui
-};
+// Buscar os nomes dos empregados, atraves do seu primeiro nome ou do sobrenome.
+// - Usar o find para encontrar o primeiro nome no banco de dados
+// Trazer todos os dados desse usuário
+//   {
+//   id: 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+//   firstName: 'Nigel',
+//   lastName: 'Nelson',
+//   managers: ['0e7b460e-acf4-4e17-bcb3-ee472265db83', 'fdb2543b-5662-46a7-badc-93d960fdc0a8'],
+//   responsibleFor: ['0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'],
+//   }
+
+const getEmployeeByName = (data, employeeName) => {
+  return data
+    .find((name = []) => employeeName.includes(name.firstName) || employeeName.includes(name.lastName));
+}
+
+console.log(getEmployeeByName(employees, 'Bethea'));
 
 module.exports = getEmployeeByName;
--- test/getEmployeeByName.test.js
+++ test/getEmployeeByName.test.js
@@ -1,7 +1,33 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const getEmployeeByName = require('../src/getEmployeeByName');
 
-const getEmployeeByName = (employeeName) => {
-  // seu código aqui
-};
-
-module.exports = getEmployeeByName;
+describe('Verifica a função getEmployeeByName', () => {
+  it('Recebe nome do empregado e retorna os dados', () => {
+    expect(getEmployeeByName(employees, 'Burl')).toEqual({
+      id: '0e7b460e-acf4-4e17-bcb3-ee472265db83',
+      firstName: 'Burl',
+      lastName: 'Bethea',
+      managers: [ '9e7d4524-363c-416a-8759-8aa7e50c0992' ],
+      responsibleFor: [
+        '0938aa23-f153-4937-9f88-4858b24d6bce',
+        'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+        'baa6e93a-f295-44e7-8f70-2bcdc6f6948d',
+        'ef3778eb-2844-4c7c-b66c-f432073e1c6b'
+      ]
+    })
+  })
+  it('Recebe sobrenome do empregado e retorna os dados', () => {
+    expect(getEmployeeByName(employees, 'Bethea')).toEqual({
+      id: '0e7b460e-acf4-4e17-bcb3-ee472265db83',
+      firstName: 'Burl',
+      lastName: 'Bethea',
+      managers: [ '9e7d4524-363c-416a-8759-8aa7e50c0992' ],
+      responsibleFor: [
+        '0938aa23-f153-4937-9f88-4858b24d6bce',
+        'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+        'baa6e93a-f295-44e7-8f70-2bcdc6f6948d',
+        'ef3778eb-2844-4c7c-b66c-f432073e1c6b'
+      ]
+    })
+  })
+})
